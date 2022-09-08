bdf3964afcad3a6388184e4c3a2d306557df85c1
NathanMaced <nathanmacedo02@gmail.com>
2022-09-08 19:49:31 -0300

    Att

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -3,12 +3,19 @@ package br.com.etecia.skoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    // Declarar as variaveis globais que irão representar os objetos xml
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Abrindo a janela activity_main
+
         setContentView(R.layout.activity_main);
     }
 }
