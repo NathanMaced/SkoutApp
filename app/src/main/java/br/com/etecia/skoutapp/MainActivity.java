51e634ce880a4851e697b2a96cd808177187976c
NathanMaced <nathanmacedo02@gmail.com>
2022-09-08 22:01:24 -0300

    Att

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.skoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,9 +45,17 @@ public class MainActivity extends AppCompatActivity {
 
                 //validadando o usuario
 
-                if (usuario.equals("etecia") && senha.equals("etecia")){
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(),
+                            MenuPrincipalActivity.class));
+                    finish();
 
-                } else {Toast.makeText(getApplicationContext(),"Usuario ou senha ivalidos!",Toast.LENGTH_SHORT).show();}
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuario ou senha ivalidos!", Toast.LENGTH_SHORT).show();
+                    edtUsuario.setText("");
+                    edtSenha.setText("");
+                    edtUsuario.requestFocus();
+                }
 
 
             }
